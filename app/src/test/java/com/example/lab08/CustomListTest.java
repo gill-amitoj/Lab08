9333d0e69d026b380ef744669d9b1f9a851ac766
gill-amitoj <amitoj3@ualberta.ca>
2025-10-31 13:58:27 -0600

    checking error for delte city

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -17,4 +17,18 @@ public class CustomListTest {
 // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        assertTrue(list.hasCity(calgary));
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
+        // ensure other city is still present
+        assertTrue(list.hasCity(edmonton));
+    }
 }
