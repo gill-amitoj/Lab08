8cd7ba0b4103ec21116a9f38151a33be297829b3
gill-amitoj <amitoj3@ualberta.ca>
2025-10-31 14:08:27 -0600

    checking error for counting cities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -31,4 +31,18 @@ public class CustomListTest {
         // ensure other city is still present
         assertTrue(list.hasCity(edmonton));
     }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+
+        assertEquals(0, list.countCities());
+        list.addCity(calgary);
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
+
+        list.deleteCity(calgary);
+        assertEquals(1, list.countCities());
+    }
 }
